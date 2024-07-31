15c40b51bc43ad86d1604b91cf82c40e7c9fe58a
wliang <wliang@infervision.com>
2024-07-31 23:00:17 +0700

    feat: 前后端对接

--- front/pages/index/index.js
+++ front/pages/index/index.js
@@ -46,26 +46,77 @@ Page({
   
   // 上传语音文件并转换为文本
   uploadVoice(filePath) {
-    wx.uploadFile({
-      url: 'YOUR_SERVER_URL', // 替换为你的服务器地址
-      filePath: filePath,
-      name: 'file',
-      success: (res) => {
-        const data = JSON.parse(res.data);
-        if (data.text) {
-          this.setData({
-            voices: [...this.data.voices, { type: 'text', content: data.text }]
+    wx.request({
+      url: 'http://localhost:3000/getAccessToken', 
+      success: function (res) {
+        const accessToken = res.data.access_token;
+        if (accessToken) {
+          console.log('accessToken', filePath,accessToken)
+              
+        // 上传录音文件到微信服务器
+          wx.uploadFile({
+            url: `https://api.weixin.qq.com/cgi-bin/media/upload?access_token=${accessToken}&type=voice`,
+            filePath: filePath,
+            name: 'file',
+            success: (uploadRes) => {
+              const data = JSON.parse(uploadRes.data);
+              console.log('data',data)
+              const mediaId = data.media_id;
+              if (mediaId) {
+                // 调用语音转文字接口
+                wx.request({
+                  url: `https://api.weixin.qq.com/cgi-bin/media/voice/addvoicetorecofortext?access_token=${accessToken}&format=mp3&voice_id=${mediaId}&lang=zh_CN`,
+                  method: 'POST',
+                  success: function (recoRes) {
+                    if (recoRes.data.errcode === 0) {
+                      // 获取结果
+                      const recognitionResult = recoRes.data.result;
+                      that.setData({ recognitionResult: recognitionResult });
+                    } else {
+                      console.error('Failed to recognize voice', recoRes.data);
+                    }
+                  },
+                  fail: function (err) {
+                    console.error('Failed to recognize voice', err);
+                  }
+                });
+              } else {
+                console.error('Failed to upload voice', data);
+              }
+            },
+            fail: (err) => {
+              console.error('Failed to upload voice', err);
+            }
           });
+        } else {
+          console.error('Failed to get access token');
         }
       },
-      fail: (error) => {
-        console.error('Upload failed', error);
-        wx.showToast({
-          title: '上传失败',
-          icon: 'none'
-        });
+      fail: function (err) {
+        console.error('Failed to get access token', err);
       }
-    });
+    })
+
+    // wx.uploadFile({
+    //   url: 'http://localhost:3000/getAccessToken', 
+    //   filePath: filePath,
+    //   name: 'file',
+    //   success: (res) => {
+    //     const data = JSON.parse(res.data);
+    //     if (data.text) {
+    //       this.setData({
+    //         voices: [...this.data.voices, { type: 'text', content: data.text }]
+    //       });
+    //     }
+    //   },
+    //   fail: (error) => {
+    //     console.error('Upload failed', error);
+    //     wx.showToast({
+    //       title: '上传失败',
+    //       icon: 'none'
+    //     });
+    //   }
+    // });
   },
   
   // 开始计时器
--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+const express = require('express');
 const axios = require('axios');
 
+require('dotenv').config(); 
+
+const app = express();
+const port = 3000;
+
+const {APP_ID, APP_SECRET} = process.env
+
 // 替换为你的 AppID 和 AppSecret
-const appId = 'YOUR_APP_ID';
-const appSecret = 'YOUR_APP_SECRET';
+const appId = APP_ID;
+const appSecret = APP_SECRET;
 
 // 获取 ACCESS_TOKEN 的函数
 async function getAccessToken() {
@@ -21,8 +29,18 @@ async function getAccessToken() {
   }
 }
 
-// 示例调用
-getAccessToken().then(accessToken => {
-  console.log('Access Token:', accessToken);
+// 创建一个 API 端点来获取 ACCESS_TOKEN
+app.get('/getAccessToken', async (req, res) => {
+  console.log('接口测试')
+  const accessToken = await getAccessToken();
+  if (accessToken) {
+    res.send({ access_token: accessToken });
+  } else {
+    res.status(500).send({ error: 'Failed to get access token' });
+  }
 });
 
+// 启动服务器
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
